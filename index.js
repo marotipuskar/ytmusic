1790a578ba4c0326528a80dbde7e9a7e6e0f3341
marotipuskar <119912274+marotipuskar@users.noreply.github.com>
2023-01-03 09:57:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,208 @@
+// document.querySelector(".div1").addEventListener("click",function(){
+//     // var audio=new Audio("Vitthal Majha Mi.mp3")
+//     // audio.play();
+// });
+var audio = new Audio("Vitthal.mp3");
+document.querySelector(".playbuton").addEventListener("click",function(){
+    audio.play();
+}); 
+var audio = new Audio("Vitthal.mp3");
+document.querySelector(".pause").addEventListener("click",function(){
+    audio.pause();
+}); 
+
+
+
+var audio1=new Audio("Achyutam Keshavam(PagalWorld.com.se).mp3");
+document.querySelector(".playbuton2").addEventListener("click",function(){
+    audio1.play();
+});
+var audio1=new Audio("Achyutam Keshavam(PagalWorld.com.se).mp3");
+document.querySelector(".pause2").addEventListener("click",function(){
+    audio1.pause();
+});
+
+
+var audio3=new Audio("yt1s.com - Hum Katha Sunaate Ram Sakal Gun Dhaam Ki.mp3");
+document.querySelector(".playbuton3").addEventListener("click",function(){
+    audio3.play();
+});
+var audio3=new Audio("yt1s.com - Hum Katha Sunaate Ram Sakal Gun Dhaam Ki.mp3");
+document.querySelector(".pause3").addEventListener("click",function(){
+    audio3.pause();
+});
+
+var audio4=new Audio("hanuman chalisa.mp3");
+document.querySelector(".playbuton4").addEventListener("click",function(){
+    audio4.play();
+});
+var audio4=new Audio("hanuman chalisa.mp3");
+document.querySelector(".pause4").addEventListener("click",function(){
+    audio4.pause();
+});
+
+var audio5=new Audio("yt1s.com - Har Har Shambhu Shiv Mahadeva feat Abhilipsa Panda.mp3");
+document.querySelector(".playbuton5").addEventListener("click",function(){
+    audio5.play();
+});
+var audio5=new Audio("yt1s.com - Har Har Shambhu Shiv Mahadeva feat Abhilipsa Panda.mp3");
+document.querySelector(".pause5").addEventListener("click",function(){
+    audio5.pause();
+});
+
+var audio6=new Audio("yt1s.com - Deva Ho Deva.mp3");
+document.querySelector(".playbuton6").addEventListener("click",function(){
+    audio6.play();
+});
+var audio6=new Audio("yt1s.com - Deva Ho Deva.mp3");
+document.querySelector(".pause6").addEventListener("click",function(){
+    audio6.pause();
+});
+
+var audio7=new Audio("yt1s.com - Govinda Namam Full Song  Sri Venkatesham Manase Smarami.mp3");
+document.querySelector(".playbuton7").addEventListener("click",function(){
+    audio7.play();
+});
+var audio7=new Audio("yt1s.com - Govinda Namam Full Song  Sri Venkatesham Manase Smarami.mp3");
+document.querySelector(".pause7").addEventListener("click",function(){
+    audio7.pause();
+});
+
+
+
+var audio8=new Audio("yt1s.com - Tujhe Na Dekhu Toh  Kumar Sanu  Alka Yagnik  Rang  1993.mp3");
+document.querySelector(".playbuton8").addEventListener("click",function(){
+    audio8.play();
+});
+var audio8=new Audio("yt1s.com - Tujhe Na Dekhu Toh  Kumar Sanu  Alka Yagnik  Rang  1993.mp3");
+document.querySelector(".pause8").addEventListener("click",function(){
+    audio8.pause();
+});
+
+var audio9=new Audio("yt1s.com - Lyrical Hamein Tumse Hua Hai Pyar  Ab Tumhare Hawale Watan SathiyoAkshay KumarDivya Khosla Kumar.mp3");
+document.querySelector(".playbuton9").addEventListener("click",function(){
+    audio9.play();
+});
+var audio9=new Audio("yt1s.com - Lyrical Hamein Tumse Hua Hai Pyar  Ab Tumhare Hawale Watan SathiyoAkshay KumarDivya Khosla Kumar.mp3");
+document.querySelector(".pause9").addEventListener("click",function(){
+    audio9.pause();
+});
+
+var audio10=new Audio("yt1s.com - Ved Lavlay.mp3");
+document.querySelector(".playbuton10").addEventListener("click",function(){
+    audio10.play();
+});
+var audio10=new Audio("yt1s.com - Ved Lavlay.mp3");
+document.querySelector(".pause10").addEventListener("click",function(){
+    audio10.pause();
+});
+
+var audio11=new Audio("yt1s.com - Jugraafiya  Full Video  Super 30  Hrithik Roshan  Mrunal Thakur  Udit Narayan  Shreya Ghoshal.mp3");
+document.querySelector(".playbuton11").addEventListener("click",function(){
+    audio11.play();
+});
+var audio11=new Audio("yt1s.com - Jugraafiya  Full Video  Super 30  Hrithik Roshan  Mrunal Thakur  Udit Narayan  Shreya Ghoshal.mp3");
+document.querySelector(".pause11").addEventListener("click",function(){
+    audio11.pause();
+});
+
+var audio12=new Audio("yt1s.com - Kesariya From Brahmastra.mp3");
+document.querySelector(".playbuton12").addEventListener("click",function(){
+    audio12.play();
+});
+var audio12=new Audio("yt1s.com - Kesariya From Brahmastra.mp3");
+document.querySelector(".pause12").addEventListener("click",function(){
+    audio12.pause();
+});
+
+var audio13=new Audio("yt1s.com - Tera Fitoor.mp3");
+document.querySelector(".playbuton13").addEventListener("click",function(){
+    audio13.play();
+});
+var audio13=new Audio("yt1s.com - Tera Fitoor.mp3");
+document.querySelector(".pause13").addEventListener("click",function(){
+    audio13.pause();
+});
+
+var audio14=new Audio("yt1s.com - Pani Pani Shraddha Kapoor X Pani Pani Song   Edit Video.mp3");
+document.querySelector(".playbuton14").addEventListener("click",function(){
+    audio14.play();
+});
+var audio14=new Audio("yt1s.com - Pani Pani Shraddha Kapoor X Pani Pani Song   Edit Video.mp3");
+document.querySelector(".pause14").addEventListener("click",function(){
+    audio14.pause();
+});
+
+// document.querySelector(".b2").addEventListener("click",function(){
+//     alert("i got click ");
+//      Have a favorite? 
+//      Tell us which artists you like and we'll improve your recommendations 
+// });  
+
+var audio15=new Audio("Untitled.mp3");
+document.querySelector(".playbuton15").addEventListener("click",function(){
+    audio15.play();
+});
+var audio15=new Audio("Untitled.mp3");
+document.querySelector(".pause15").addEventListener("click",function(){
+    audio15.pause();
+});
+
+var audio16=new Audio("WhatsApp Video 2023-01-02 at 17.21.29.mp3");
+document.querySelector(".playbuton16").addEventListener("click",function(){
+    audio16.play();
+});
+var audio16=new Audio("WhatsApp Video 2023-01-02 at 17.21.29.mp3");
+document.querySelector(".pause16").addEventListener("click",function(){
+    audio16.pause();
+});
+
+var audio17=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+document.querySelector(".playbuton17").addEventListener("click",function(){
+    audio17.play(); 
+
+});
+var audio17=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+document.querySelector(".pause17").addEventListener("click",function(){
+    audio17.pause(); 
+
+});
+
+
+
+
+// var audio1=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+// document.querySelector(".pause1").addEventListener("click",function(){
+//     audio1.pause(); 
+    
+// });
+// var audio2=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+// document.querySelector(".pause2").addEventListener("click",function(){
+//     audio2.pause(); 
+    
+// });
+
+// var audio3=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+// document.querySelector(".pause3").addEventListener("click",function(){
+//     audio3.pause(); 
+    
+// });
+// var audio1=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+// document.querySelector(".pause1").addEventListener("click",function(){
+//     audio1.pause(); 
+    
+// });
+
+
+
+
+
+
+
+// var audio17=new Audio("yt1s.com - Ae mere vatan ke logon jra aankh mai bharo ro paani.mp3");
+// document.querySelector(".pause17").addEventListener("click",function(){
+//     audio17.pause(); 
+    
+// });
+
+
